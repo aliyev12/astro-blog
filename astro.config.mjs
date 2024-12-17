9bf433cf27a49a37095bb73e24cc47464ae3d07b
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-17 17:57:15 -0500

    Add SSR version of slug based article dynamic page component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  // Below configuration is for SSR site
+  // Remove it if you want the site to be static
+  // start of SSR configuration
+  output: "server",
+  // end of SSR configuration.
 });
